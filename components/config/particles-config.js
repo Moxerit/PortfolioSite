f8b9a7531359b6411e0fa5d828d120be859baa39
Chase Alber <calber207@gmail.com>
2023-01-09 15:21:06 -0700

    responsive particle.js added

--- components/config/particles-config.js
+++ components/config/particles-config.js
@@ -4,135 +4,114 @@ const particlesConfig = {
     enable: true,
     zIndex: -2
   },
-  particles: {
-    number: {
-      value: 70,
-      density: {
-        enable: false,
-        area: 1000
+  "particles": {
+    "number": {
+      "value": 60,
+      "density": {
+        "enable": true,
+        "value_area": 800
       }
     },
-    color: {
-      value: "#A82428"
+    "color": {
+      "value": "#a82428"
     },
-    shape: {
-      type: "circle",
-      options: {
-        polygon: {
-          sides: 5
-        },
-        image: {
-          
-          width: 100,
-          height: 100
-        }
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    stroke: {
-      width: 0
-    },
-    opacity: {
-      value: 1,
-      random: false,
-      animation: {
-        enable: false,
-        speed: 3,
-        minimumValue: 0.1,
-        sync: false
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
       }
     },
-    size: {
-      value: 2,
-      minimumvalue: 1,
-      random: true,
-      animation: {
-        enable: false,
-        speed: 20,
-        minimumValue: 1,
-        sync: false
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
       }
     },
-    links: {
-      enable: true,
-      distance: 130,
-      color: "random",
-      blink: false,
-      consent: false,
-      opacity: 0.5,
-      width: 2,
-      triangles: {
-        enable: true,
-        opacity: 0.05
-      }
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#a82428",
+      "opacity": 0.4,
+      "width": 1
     },
-    move: {
-      warp: false,
-      enable: true,
-      speed: 3,
-      direction: "none",
-      random: false,
-      straight: false,
-      outMode: "out",
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
       }
     }
   },
-  interactivity: {
-    detectsOn: "window",
-    events: {
-      onClick: {
-        enable: false,
-        mode: ""
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
       },
-      onHover: {
-        enable: true,
-        mode: "",
-        parallax: {
-          enable: true,
-          force: 10,
-          smooth: 15
-        }
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
       },
-      
+      "resize": true
     },
-    modes: {
-      grab: {
-        distance: 50,
-        links: {
-          opacity: 1
+    "modes": {
+      "grab": {
+        "distance": 250,
+        "line_linked": {
+          "opacity": 1
         }
       },
-      connect: {
-        radius: 20
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
       },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 0.8
+      "repulse": {
+        "distance": 400,
+        "duration": 0.6
       },
-      repulse: {
-        distance: 50
+      "push": {
+        "particles_nb": 4
       },
-      push: {
-        quantity: 4
-      },
-      remove: {
-        quantity: 2
+      "remove": {
+        "particles_nb": 2
       }
     }
   },
-  detectRetina: true,
-  background: {
-    color: "#0e151a",
-    image: "",
-    position: "50% 50%",
-    repeat: "no-repeat",
-    size: "cover"
-  }
+  "retina_detect": true
 };
 
 
